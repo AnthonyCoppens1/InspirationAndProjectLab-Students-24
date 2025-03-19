b5bf8f21289ada2ed3df6fcf959303a78f3d9356
AnthonyCoppens1 <anthonycoppens95@gmail.com>
2025-03-20 00:24:34 +0100

    Week6

---
+++ Week6/ADI/Hospital/Hospital/Hospital.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    public class Hospital
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+
+        private Data data = new Data();
+
+        public Hospital(string name)
+        {
+            Name = name;
+            ID = data.InsertHospital(this);
+        }
+
+        public Hospital(string name, List<Person> people)
+        {
+            Name = name;
+            ID = data.InsertHospital(this, people);
+        }
+
+        public void AddPerson(Person person)
+        {
+            data.AddPeopleToHospital(person.ID, this.ID);
+        }
+
+        public List<Patient> GetPatients()
+        {
+            return data.SelectPatients(this.ID);
+        }
+
+
+        public List<Person> GetStaff()
+        {
+            return data.SelectStaff(this.ID);
+        }
+
+        public override string ToString()
+        {
+            string s = "HOSPITAL " + Name + "\n";
+            s += "--- PATIENTS\n";
+            foreach (Person person in GetPatients())
+            {
+                s += "- " + person + "\n";
+            }
+
+            s += "\n--- STAFF\n";
+            foreach (Person person in GetStaff())
+            {
+                s += "- " + person + "\n";
+            }
+            return s;
+        }
+    }
+}
